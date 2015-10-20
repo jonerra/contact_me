eff626f017c27f5677a1ab0cc4062da1b01d0061
Jon <jonerra23@aim.com>
2015-10-20 19:15:33 +0000

    Add contact create view

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    name = models.CharField(max_length=300)
+    email = models.CharField(max_length=300)
+    message = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
