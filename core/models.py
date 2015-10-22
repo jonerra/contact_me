ee281dc72533fbc0d44f8ade065a19590172e7cc
Jon <jonerra23@aim.com>
2015-10-22 21:24:39 +0000

    small fix

--- core/models.py
+++ core/models.py
@@ -2,17 +2,16 @@ from django.db import models
 from django.core.exceptions import ValidationError
 
 TITLE_CHOICES = (
-(0, ' '),
-(1, 'Ms.'),
-(2, 'Mrs.'),
-(3, 'Mr.'),
-(4, 'Dr.'),
+(0, 'Ms.'),
+(1, 'Mrs.'),
+(2, 'Mr.'),
+(3, 'Dr.'),
 )
 
 # Create your models here.
 class Message(models.Model):
     subject = models.CharField(max_length=300)
-    title = models.IntegerField(choices=TITLE_CHOICES, default=0)
+    title = models.IntegerField(choices=TITLE_CHOICES)
     name = models.CharField(max_length=300)
     email = models.EmailField(max_length=300, blank=False, unique=False)
     message = models.TextField(null=True, blank=True)
