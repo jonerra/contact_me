9548c786b0b2f9dcbd1aa13ebd610e6f57fbd470
Jon <jonerra23@aim.com>
2015-10-22 14:19:26 +0000

    Add honorifics

--- core/models.py
+++ core/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
+TITLE_CHOICES = (
+(0, ' '),
+(1, 'Ms.'),
+(2, 'Mrs.'),
+(3, 'Mr.'),
+(4, 'Dr.'),
+)
+
 # Create your models here.
 class Message(models.Model):
-    title = models.CharField(max_length=300)
+    title = models.IntegerField(choices=TITLE_CHOICES, default=0)
     name = models.CharField(max_length=300)
     email = models.CharField(max_length=300)
     message = models.TextField(null=True, blank=True)
