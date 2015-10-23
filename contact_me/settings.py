b5f2dfb5352362d1ff3f733c596a6a4e67e0a8aa
Jon <jonerra23@aim.com>
2015-10-23 20:17:50 +0000

    Added django-extensions

--- contact_me/settings.py
+++ contact_me/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import django_extensions
 
 ON_HEROKU = os.getenv('ON_HEROKU', False)
 
